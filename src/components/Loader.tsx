b2bf5d27a4e44dcfbef397cc668be3169620d9a9
Dnikulshin <d.nikulshin.dev@gmail.com>
2025-06-09 15:22:20 +0300

    fix: Loader css

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 
-export const Loader = () => {
+export const Loader = ({ type }: { type: "h-screen" | "h-full" }) => {
   return (
-    <div className="flex h-screen justify-center items-center text-blue-500 font-bold text-center">
+    <div
+      className={`flex ${type} min-h-[200px] justify-center items-center text-blue-500 font-bold text-center`}
+    >
       <div className="loader"></div>
     </div>
   );
